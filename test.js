9c4f72ef28dbb4a69707ffb4104c1ed50befbed4
Tim Pinington <timpinington@gmail.com>
2014-01-03 20:58:53 +0000

    test for sourcemaps

--- test.js
+++ test.js
@@ -17,3 +17,18 @@ it('should preprocess CSS using Rework', function (cb) {
 		contents: new Buffer('.logo{background-image:url(\'component.png\') at-2x;width:289px;height:113px;}')
 	}));
 });
+
+it('should preprocess CSS using Rework', function (cb) {
+	var stream = rework(rework.at2x(), true);
+	stream.on('data', function (data) {
+		assert.equal(
+			data.contents.toString(),
+			'.logo {\n  background-image: url(\'component.png\');\n  width: 289px;\n  height: 113px;\n}\n\n@media (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (-webkit-min-device-pixel-ratio: 1.5), (min-device-pixel-ratio: 1.5), (min-resolution: 144dpi), (min-resolution: 1.5dppx) {\n  .logo {\n    background-image: url("component@2x.png");\n    background-size: contain;\n  }\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2VuZXJhdGVkLmNzcyIsInNvdXJjZXMiOlsic291cmNlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUFNLHNDQUEyQztFQUFDLFlBQVc7RUFBQyxhQUFZIn0= */'
+		);
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		contents: new Buffer('.logo{background-image:url(\'component.png\') at-2x;width:289px;height:113px;}')
+	}));
+});
