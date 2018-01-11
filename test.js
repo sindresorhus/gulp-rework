3b491a97cb9e2350eb2beaac0c4a5ca31ffc982a
johanblumenberg <johan.blumenberg@gmail.com>
2018-01-11 15:24:28 +0100

    Adding support for css sourcemaps

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 const at2x = require('rework-plugin-at2x');
 const Vinyl = require('vinyl');
+const sourcemap = require('source-map');
 const rework = require('.');
 
 it('should preprocess CSS using Rework', cb => {
@@ -35,4 +36,73 @@ it('should support Source Map', cb => {
 	stream.end(new Vinyl({
 		contents: Buffer.from('.logo{background-image:url(\'component.png\') at-2x;width:289px;height:113px;}')
 	}));
+});
+
+function identityPlugin() {
+	return () => {};
+}
+
+it('should support gulp-sourcemap', cb => {
+	const map = new sourcemap.SourceMapGenerator({file: 'src/styles/logo.css'});
+	map.addMapping({
+		generated: {line: 1, column: 0},
+		source: 'src/styles/mixin.scss',
+		original: {line: 33, column: 2},
+		name: 'abc'
+	});
+
+	const file = new Vinyl({
+		path: '/Users/me/code/src/styles/logo.css',
+		base: '/Users/me/code',
+		contents: Buffer.from('.logo{width:289px;}')
+	});
+	file.sourceMap = map.toJSON();
+
+	const stream = rework(identityPlugin());
+
+	stream.on('data', data => {
+		try {
+			assert.equal(
+				data.contents.toString(),
+				'.logo {\n  width: 289px;\n}'
+			);
+
+			const map = new sourcemap.SourceMapConsumer(data.sourceMap);
+			assert.deepEqual(
+				map.originalPositionFor({line: 2, column: 2}),
+				{line: 33, column: 2, source: 'src/styles/mixin.scss', name: 'abc'});
+
+			cb();
+		} catch (err) {
+			cb(err);
+		}
+	});
+
+	stream.write(file);
+});
+
+it('should not override given options', cb => {
+	const map = new sourcemap.SourceMapGenerator({file: 'src/styles/logo.css'});
+	const file = new Vinyl({
+		path: 'src/styles/logo.css',
+		contents: Buffer.from('.logo{width:289px;}')
+	});
+	file.sourceMap = map.toJSON();
+	const options = {};
+
+	const stream = rework(identityPlugin(), options);
+
+	stream.on('data', () => {
+		try {
+			assert.deepEqual(
+				options, {}
+			);
+
+			cb();
+		} catch (err) {
+			cb(err);
+		}
+	});
+
+	stream.write(file);
 });
