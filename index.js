4990bc655367bff7bca09a40023533a78b6f07bd
johanblumenberg <johan.blumenberg@gmail.com>
2018-05-05 13:34:26 +0200

    Add support for CSS source maps (#13)

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const _ = require('lodash');
 const rework = require('rework');
 const PluginError = require('plugin-error');
 const Buffer = require('safe-buffer').Buffer;
+const applySourceMap = require('vinyl-sourcemaps-apply');
 
 const lastIsObject = _.flowRight(_.isPlainObject, _.last);
 
 module.exports = function () {
 	const args = [].slice.call(arguments);
-	const options = lastIsObject(args) ? args.pop() : {};
+	const options = Object.assign({}, lastIsObject(args) ? args.pop() : {});
 	const plugins = args;
 
 	return through.obj((file, enc, cb) => {
@@ -26,7 +27,23 @@ module.exports = function () {
 		try {
 			const ret = rework(file.contents.toString(), {source: file.path});
 			plugins.forEach(ret.use.bind(ret));
-			file.contents = Buffer.from(ret.toString(options));
+
+			if (file.sourceMap) {
+				options.sourcemap = true;
+				options.inputSourcemap = false;
+				options.sourcemapAsObject = true;
+			}
+
+			const result = ret.toString(options);
+			if (file.sourceMap) {
+				file.contents = Buffer.from(result.code);
+				result.map.file = file.relative;
+				result.map.sources = result.map.sources.map(() => file.relative);
+				applySourceMap(file, result.map);
+			} else {
+				file.contents = Buffer.from(result);
+			}
+
 			cb(null, file);
 		} catch (err) {
 			cb(new PluginError('gulp-rework', err, {fileName: err.filename || file.path}));
